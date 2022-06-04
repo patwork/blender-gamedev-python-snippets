c76fcae5d7bddc9231a9c63cef95589a766bb3f1
patwork <patwork@gmail.com>
2022-06-04 10:21:44 +0200

    Create sync-objects-data-names.py

---
+++ sync-objects-data-names.py
@@ -0,0 +1,13 @@
+# sync objects data names
+# https://blender.stackexchange.com/questions/158913/how-to-rename-an-objects-mesh-data-with-python
+
+import bpy
+
+for obj in bpy.data.objects:
+    if obj.data:
+        name = "%s.%s" % (obj.name, obj.type.lower())
+        if obj.data.name != name:
+            print("- %s -> %s" % (obj.data.name, name))
+            obj.data.name = name
+
+# EoF
